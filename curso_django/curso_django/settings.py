18cb98a55fa1cf126d94cb916c550db06af3440b
Raphael Britto <raphael.britto@gmail.com>
2018-05-25 18:33:59 -0300

    criacao app courses

--- curso_django/curso_django/settings.py
+++ curso_django/curso_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'curso_django.core',
+    'curso_django.courses',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'curso_django', 'media')
