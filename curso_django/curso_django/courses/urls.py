c13f7ebe8255da14c760c34f5c902ca5ca9f7ccf
Raphael Britto <raphael.britto@gmail.com>
2018-06-04 19:17:19 -0300

    adicionar pk detalhe curso

--- curso_django/curso_django/courses/urls.py
+++ curso_django/curso_django/courses/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from curso_django.courses import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^(?P<pk>\d+)/$', views.details, name='details'),
 ]
